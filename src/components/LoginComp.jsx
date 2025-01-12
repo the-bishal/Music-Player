57665cb666036d95d49ffd86a79c72d6a805974e
Aazad-Tiwari <aazadtiwari15@gmail.com>
2025-01-12 23:35:05 +0530

    implement login functionality with form validation in LoginComp

--- src/components/LoginComp.jsx
+++ src/components/LoginComp.jsx
@@ -2,16 +2,31 @@ import React from 'react'
 import Input from '../components/Input'
 import Tiger from '../assets/images/tiger.png'
 import { useNavigate } from 'react-router'
+import { useForm } from 'react-hook-form'
+import authService from '../music_backend/auth'
 
 const LoginComp = () => {
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const {register, handleSubmit, formState:{errors}} = useForm()
+  
 
   const handleNavigate = () => {
     navigate("/signup")
   }
 
-  const LoginUser = (e) => {}
+  const loginUser = async(data) => {
+    try {
+      const userData = await authService.loginUser(data);
+      if (!userData || userData instanceof Error) {
+        throw new Error(userData);
+      }
+
+      navigate("../pages/Homepage.jsx")
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div className="h-screen flex flex-col md:flex-row">
@@ -28,7 +43,7 @@ const LoginComp = () => {
       <div className="md:w-1/2 w-full bg-transparent flex items-center justify-center p-6">
         <div className="w-full max-w-md">
           <h2 className="text-3xl md:text-5xl font-bold text-white mb-12 text-center">Login</h2>
-          <form className='shadow shadow-slate-700 p-5' onSubmit={LoginUser} >
+          <form className='shadow shadow-slate-700 p-5' onSubmit={handleSubmit(loginUser)} >
             <div className="mb-6">
               <Input 
                 label="Email"
@@ -36,8 +51,13 @@ const LoginComp = () => {
                 placeholder="Email"
                 className="w-full px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
                 labelClass="block text-gray-400 text-sm font-bold mb-2"
+                {...register('Email', {
+                  required: 'Email is required'
+                })}
               />
             </div>
+            {errors.Email && <p className="text-red-500 mb-1">{errors.Email.message}</p>}
+            <div className="mt-2"></div>
             <div className="mb-6">
               <Input 
                 label="Password"
@@ -45,8 +65,12 @@ const LoginComp = () => {
                 placeholder="Password"
                 className="w-full px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
                 labelClass="block text-gray-400 text-sm font-bold mb-2"
+                {...register('password', {
+                  required: 'password is required'
+                })}
               />
             </div>
+            {errors.password && <p className="text-red-500 -mt-5 mb-4">{errors.password.message}</p>}
             <div className="flex items-center justify-between">
               <button 
                 className="bg-green-500 hover:bg-green-700 text-white font-bold mt-4 py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" 
