1a9a005a7dc7d0b63cd9deba72c593eda7fcb88c
Aazad-Tiwari <aazadtiwari15@gmail.com>
2025-01-14 23:48:55 +0530

    add index.js to export main page components

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Homepage from "./Homepage.jsx";
+import Login from "./Login.jsx";
+import ShowAll from "./ShowAll.jsx";
+import Signup from "./Signup.jsx"
+
+export {Homepage, Login, ShowAll, Signup}
